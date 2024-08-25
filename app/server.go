f4138d74f90cbe0c3471d08b292cb60da824382c
litblock <37122962+litblock@users.noreply.github.com>
2024-08-24 20:16:54 -0400

    Update server.go

--- app/server.go
+++ app/server.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
+	"strings"
 )
+
+//var Commands = map[string]func([]Value) Value{
+//	"PING": ping
+//	"ECHO": echo
+//}
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
@@ -14,25 +21,35 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	connection, err := l.Accept()
+
 	for {
+		connection, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		handleRequest(connection)
+		go handleRequest(connection)
 	}
 }
 
 func handleRequest(connection net.Conn) {
 	buf := make([]byte, 1024)
-	_, err := connection.Read(buf)
-	if err != nil {
-		fmt.Println("Error while trying to read:", err.Error())
+	for {
+		n, err := connection.Read(buf)
+		if err != nil {
+			fmt.Println("Error while trying to read:", err.Error())
+		}
+		log.Println("read: ", n)
+		parseToString(buf)
+		//_, err = connection.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			fmt.Println("Error trying to write", err.Error())
+		}
 	}
-	//fmt.Println(in)
-	_, err = connection.Write([]byte("+PONG\r\n"))
-	if err != nil {
-		fmt.Println("Error trying to write", err.Error())
-	}
+}
+
+func parseToString(buf []byte) {
+	input := (string)(buf)
+	split := strings.Split(input, "\r\n")
+	log.Println(split)
 }
